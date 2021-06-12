b263cf79e894a0900484a23cf6157bffebd94fe7
Kevin Colour <kecolour@gmail.com>
2021-06-12 01:25:47 -0700

    Bullet firing

--- src/first_level.c
+++ src/first_level.c
@@ -88,7 +88,6 @@ void	*first_level_update(SDL_UNUSED t_context *context, SDL_UNUSED void *level, 
 
 	SDLX_RenderQueue_add(scene->queue, &(scene->player));
 	SDLX_RenderQueue_add(scene->queue, &(scene->crosshair));
-	SDLX_RenderQueue_add(scene->queue, &(scene->bullet.sprite));
 	scene->crosshair.angle = (SDL_atan2(g_GameInput.GameInput.primary.x - 130, 140 - g_GameInput.GameInput.primary.y) * 180 / M_PI) - 45;
 
 	if (scene->bullet.fired == SDL_FALSE)
@@ -106,9 +105,28 @@ void	*first_level_update(SDL_UNUSED t_context *context, SDL_UNUSED void *level, 
 		scene->bullet.fired = SDL_TRUE;
 		angle = SDL_atan2(g_GameInput.GameInput.primary.x - (scene->bullet.sprite._dst.x + 4), (scene->bullet.sprite._dst.y + 16) - g_GameInput.GameInput.primary.y);
 
-		scene->bullet.vel.x = SDL_sin(angle) * 5;
-		scene->bullet.vel.y = SDL_cos(angle) * -5;
+		scene->bullet.vel.x = SDL_sin(angle) * 9;
+		scene->bullet.vel.y = SDL_cos(angle) * -9;
 	}
+
+	SDL_Rect	play_area;
+	SDL_Rect	b_real;
+
+	play_area = (SDL_Rect){0, 0, 32 * 16, 32 * 16};
+	b_real = scene->bullet.sprite._dst;
+
+	b_real.x *= 2;
+	b_real.y *= 2;
+	b_real.w *= 2;
+	b_real.h *= 2;
+
+	if (SDL_HasIntersection(&(b_real), &(play_area)) == SDL_FALSE)
+	{
+		scene->bullet.fired = SDL_FALSE;
+		scene->bullet.sprite._dst = (SDL_Rect){130, 7 * 16 + 8, 16, 16};
+	}
+	if (scene->bullet.fired)
+		SDLX_RenderQueue_add(scene->queue, &(scene->bullet.sprite));
 
 	SDLX_Button_Update(&(scene->pause));
 
