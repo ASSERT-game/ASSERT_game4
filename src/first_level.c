c791a6eeb0c2911b678999f6079d73fae18ae52c
Kevin Colour <kecolour@gmail.com>
2021-06-11 18:47:54 -0700

    Crosshair

--- src/first_level.c
+++ src/first_level.c
@@ -19,6 +19,9 @@ typedef struct	s_firt_level
 	SDLX_RenderQueue	*queue;
 
 	SDLX_button			pause;
+
+	SDLX_Sprite			player;
+	SDLX_Sprite			crosshair;
 }				t_firt_level;
 
 void	*first_level_init(t_context *context, SDL_UNUSED void *level, SDL_UNUSED int tick)
@@ -34,6 +37,18 @@ void	*first_level_init(t_context *context, SDL_UNUSED void *level, SDL_UNUSED in
 	scene->queue = &(context->queue);
 
 	SDLX_Button_Init(&(scene->pause), fetch_ui_sprite, PAUSE_NORM, (SDL_Rect){256 - 21, 5, 16, 16}, scene->queue);
+
+	scene->player = SDLX_Sprite_Static(ASSETS"bunny.png");
+	scene->player.dst = &(scene->player._dst);
+	scene->player._dst = (SDL_Rect){7 * 16, 7 * 16, 40, 40};
+	scene->crosshair = SDLX_Sprite_Static(ASSETS"crosshair.png");
+	scene->crosshair.dst = &(scene->crosshair._dst);
+	scene->crosshair.dst = &(scene->crosshair._dst);
+	scene->crosshair._dst = (SDL_Rect){130, 100, 40, 40};
+	scene->crosshair.center = &(scene->crosshair._center);
+	scene->crosshair._center.x = 0;
+	scene->crosshair._center.y = 80;
+	scene->crosshair.angle = 0;
 
 	context->close_fn = first_level_close;
 	context->update_fn = first_level_update;
@@ -59,6 +74,10 @@ void	*first_level_update(SDL_UNUSED t_context *context, SDL_UNUSED void *level, 
 	t_firt_level	*scene;
 
 	scene = level;
+
+	SDLX_RenderQueue_add(scene->queue, &(scene->player));
+	SDLX_RenderQueue_add(scene->queue, &(scene->crosshair));
+	scene->crosshair.angle = (SDL_atan2(g_GameInput.GameInput.primary.x - 130, 140 - g_GameInput.GameInput.primary.y) * 180 / M_PI) - 45;
 
 	SDLX_Button_Update(&(scene->pause));
 
